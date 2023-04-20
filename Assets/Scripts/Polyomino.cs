de5bbdb4cb9a8e70a0ec96e0491961976a0f3e0c
magur0don <magurodon.10000yen@gmail.com>
2023-04-20 16:17:13 +0900

    ポリオミノの配列化

--- Assets/Scripts/Polyomino.cs
+++ Assets/Scripts/Polyomino.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering.Universal;
 using UnityEngine;
 
 public class Polyomino : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Poryiminoの形を配列で表現
+    /// 例えば
+    /// 0,0,0
+    /// 0,1,0
+    /// 1,1,1
+    /// だと凸のPoryominoとする
+    /// 座標は左上から数え始める
+    /// 0,0,0
+    /// 0,1,0
+    /// 0,0,0
+    /// だと1つのPolyomino
+    /// </summary>
+    public int[,] PolyominoForm = new int[3, 3]
     {
-        
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 }
+    };
+
+    private void Start()
+    {
+        SetPolyominoForm();
+        Debug.Log(PolyominoForm);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetPolyominoForm()
     {
-        
+        foreach (var mino in this.transform.GetComponentsInChildren<SpriteRenderer>())
+        {   
+            PolyominoForm[(int)mino.transform.localPosition.y+1, (int)mino.transform.localPosition.x+1] = 1;
+        }
     }
 }
---
+++ Assets/Scripts/PolyominoDungeonMaker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolyominoDungeonMaker : MonoBehaviour
+{
+
+    // DungeonMap
+    int[,] DungeonMap = new int[10, 10]; 
+
+
+    public void PolyominoDungeonMake()
+    {
+        // ‚â‚è‚½‚¢‚±‚Æ
+        // •¡”‚Ìpolyomino‚ğ—^‚¦‚ç‚ê‚é‚Æ‚»‚ê‚ğ˜AŒ‹‚·‚é
+        //
+
+
+    }
+}
